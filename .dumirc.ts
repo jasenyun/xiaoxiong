afcb732eec03aab785dc6b6653f4eb3649196450
jasen <jaseny@foxmail.com>
2023-07-30 14:01:38 +0800

    update dumires.ts

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,10 +4,7 @@ const repo = 'xiaoxiong'
 
 export default defineConfig({
   title: 'xiaoxiong',
-  mode: ' site',
-  devServer: {
-    port: 2801,
-  },
+
   base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 
