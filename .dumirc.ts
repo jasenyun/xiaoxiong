d980c6ce44e266808cd37445b153c380bc76a982
jasen <jaseny@foxmail.com>
2023-07-30 13:38:41 +0800

    add dumirc.ts

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'xiaoxiong'
+
 export default defineConfig({
+  title: 'xiaoxiong',
+  mode: ' site',
+  devServer: {
+    port: 2801,
+  },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+
+  // more config: https://d.umijs.org/config
   themeConfig: {
     name: 'xiaoxiong',
   },
